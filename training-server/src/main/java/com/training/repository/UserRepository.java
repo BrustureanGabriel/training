dcdea19fe9b87bfe0adca80a2f9295ca0c617adb
Gabriel <gabbriel236@gmail.com>
2019-03-29 17:44:05 +0200

    Commit all
    Direct way

---
+++ training-server/src/main/java/com/training/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.training.repository;
+
+import com.training.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+}
